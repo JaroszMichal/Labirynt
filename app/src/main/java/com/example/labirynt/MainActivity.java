619c5f2ebcc0ac143d2ac29f3dd4c672d848e28e
JaroszMichal <jkm@jkm.pl>
2020-10-18 02:23:48 +0200

    GameLoop

--- app/src/main/java/com/example/labirynt/MainActivity.java
+++ app/src/main/java/com/example/labirynt/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.labirynt;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Set windows to fullscreen (will hide the status bar)
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(new Game(this));
     }
 }
